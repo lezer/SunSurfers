6cb85e38af8f7062e7fcd58ab89229985d4afa38
lezer <lezerlezerlezer@gmail.com>
2015-05-23 11:03:50 +0300

    Add in Hub
    (cherry picked from commit cdd6ef942eba3d0a1dd33a5b394221ec62356a35)

--- app/src/main/java/com/lezer/sunsurfers/MainActivity.java
+++ app/src/main/java/com/lezer/sunsurfers/MainActivity.java
@@ -1,17 +1,50 @@
 package com.lezer.sunsurfers;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.app_name);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+        initNavDrawer(toolbar);
+    }
+
+    private void initNavDrawer(Toolbar toolbar) {
+
+        AccountHeader.Result accHeaderResult = new AccountHeader()
+                .withActivity(this)
+                //.withHeaderBackground(R.drawable.background_material)
+                .build();
+
+        Drawer.Result drawerResult  = new Drawer()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withAccountHeader(accHeaderResult)
+                .withActionBarDrawerToggleAnimated(true)
+                .withDisplayBelowToolbar(true)
+                .addDrawerItems(
+                        new PrimaryDrawerItem()
+                                .withName(R.string.draw_chat),
+                        new SecondaryDrawerItem()
+                                .withName(R.string.draw_map),
+                        new SecondaryDrawerItem()
+                                .withName(R.string.draw_adt)
+                )
+                .build();
     }
 }
